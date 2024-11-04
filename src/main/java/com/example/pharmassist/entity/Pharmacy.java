c31f345de7f779427dbc88482c5dad569bb65180
Balaji5902 <balaji05902@gmail.com>
2024-11-04 12:05:19 +0530

    Patient entity created

--- src/main/java/com/example/pharmassist/entity/Pharmacy.java
+++ src/main/java/com/example/pharmassist/entity/Pharmacy.java
@@ -1,9 +1,13 @@
 package com.example.pharmassist.entity;
 
+import java.util.List;
+
 import com.example.pharmassist.config.GenerateCustomId;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 
 @Entity
@@ -19,8 +23,18 @@ public class Pharmacy
 	@OneToOne(mappedBy = "pharmacy")
 	private Admin admin;
 
+	@OneToMany
+	private List<Patient> patients;
+
+
 	public Admin getAdmin() {
 		return admin;
+	}
+	public List<Patient> getPatients() {
+		return patients;
+	}
+	public void setPatients(List<Patient> patients) {
+		this.patients = patients;
 	}
 	public void setAdmin(Admin admin) {
 		this.admin = admin;
