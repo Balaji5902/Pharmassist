083396f0c50e2a800a6e68e0ce932b09de2ad723
Balaji5902 <balaji05902@gmail.com>
2024-11-08 11:52:57 +0530

    Finding the medicine based on the name or ingredient of the medicine

--- src/main/java/com/example/pharmassist/repository/MedicineRepository.java
+++ src/main/java/com/example/pharmassist/repository/MedicineRepository.java
@@ -1,6 +1,8 @@
 package com.example.pharmassist.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.pharmassist.entity.Medicine;
@@ -8,4 +10,5 @@ import com.example.pharmassist.entity.Medicine;
 public interface MedicineRepository extends JpaRepository<Medicine,String>
 {
 
+	List<Medicine> findByNameLikeIgnoreCaseOrIngredientsLikeIgnoreCase(String name, String ingredients);
 }
